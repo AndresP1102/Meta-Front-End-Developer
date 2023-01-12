d614a0ecd712d93d214c262df7048da0a982818b
AndresP1102 <89894189+AndresP1102@users.noreply.github.com>
2023-01-11 22:07:26 -0600

    Create ExerciseOperators.js

---
+++ 2- Programming with JavaScript/ExerciseOperators.js
@@ -0,0 +1,31 @@
+var score = 8;
+
+console.log("Mid-level skills:", score>0 && score<10);
+
+var timeRemaining = 0;
+var energy = 10;
+
+console.log("Game over:", timeRemaining ==0 || energy==0);
+
+num1 = 2;
+num2 = 5;
+test1 = num1%2;
+test2 = num2%2;
+result1 = test1 == 0;
+result2 = test2 == 0;
+
+console.log("is", num1, "an even number?", result1);
+console.log("is", num2, "an even number?", result2);
+
+console.log(5 + 10); // 15
+
+var now = "Now in ";
+var three = 3;
+var d = "D!";
+
+console.log(now + three + d); // "Now in 3D!"
+
+var counter = 0;
+counter+=5;
+counter+=3;
+console.log(counter); // 8
