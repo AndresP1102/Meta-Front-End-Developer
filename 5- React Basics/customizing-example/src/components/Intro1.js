1ce960de538394b6daa1afd5eba40186b9dd9354
AndresP1102 <89894189+AndresP1102@users.noreply.github.com>
2023-01-29 21:29:31 -0600

    React Basics Progress

---
+++ 5- React Basics/customizing-example/src/components/Intro1.js
@@ -0,0 +1,13 @@
+function Intro1() {
+    return (
+        <div className = "blog-post-intro">
+            <h2> I've become a React develoeper</h2>
+            <div>
+                <p>I've completed the React Basics course and I'm happy to announce that I'm a Junior React develoeper!</p>
+                <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro1
